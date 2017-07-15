f704ed9b194539a937d2890c6e0863930b50b3ce
Jacob Chaffin <jchaffin57@gmail.com>
2017-07-15 08:57:25 -0700

    project from Using  Docker book :tada: :monkey:

---
+++ cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+set -e
+
+if [ "$ENV" = 'DEV' ] ; then
+  echo -e "Running Development Server ⌨️🎧🚧 "
+  exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ] ; then
+  echo "Running Unit Tests 👷"
+  exec python "tests.py"
+else
+  echo -e "Running Production Server 🏁"
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file
+  /app/identidock.py \
+                  --callable app --stats 0.0.0.0:9091
+fi
+
+
+
+
+
+
+
